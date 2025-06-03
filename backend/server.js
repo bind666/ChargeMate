545919098408be1a9bd72a1f0d65619ce16d9d06
bind666 <144540630+bind666@users.noreply.github.com>
2025-06-03 13:09:21 +0530

    frontend link added

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ limit: "1MB", extended: true }));
 app.use(cookieParser())
 // app.use(cors());
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174" ,"https://charge-mate-five.vercel.app"],
     credentials: true,
 }))
 
